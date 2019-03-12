89b39423d6a45e71e6802cae8709ac514b172d57
Paweł <paweltraw@gmail.com>
2019-03-12 22:26:54 +0100

    first dice created

--- app/src/main/java/com/example/boardgamehelper/DiceActivity.kt
+++ app/src/main/java/com/example/boardgamehelper/DiceActivity.kt
@@ -19,7 +19,7 @@ class DiceActivity : AppCompatActivity() {
             android.R.layout.simple_spinner_item
         ).also { adapter ->
             // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            adapter.setDropDownViewResource(android.R.layout.simple_list_item_single_choice)
             // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
