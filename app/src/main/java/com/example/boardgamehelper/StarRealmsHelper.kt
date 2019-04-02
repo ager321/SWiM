7a689bc7dfea28b37c234d64765ffa20974e2a68
Paweł <paweltraw@gmail.com>
2019-04-02 18:50:47 +0200

    alert dialog added

--- app/src/main/java/com/example/boardgamehelper/StarRealmsHelper.kt
+++ app/src/main/java/com/example/boardgamehelper/StarRealmsHelper.kt
@@ -1,9 +1,19 @@
 package com.example.boardgamehelper
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Color
+import android.graphics.Color.RED
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.bumptech.glide.Glide
+import android.support.v4.app.DialogFragment
+import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_star_realms_helper.*
 
 
 class StarRealmsHelper : AppCompatActivity() {
@@ -18,5 +28,51 @@ class StarRealmsHelper : AppCompatActivity() {
             .placeholder(R.drawable.black_dice_256) // any placeholder to load at start
             .override(256, 256) // resizing
             .into(imageBlob)
+
+
+        change_theme.setOnClickListener{
+            // Initialize a new instance of
+            val builder = AlertDialog.Builder(this@StarRealmsHelper)
+
+            // Set the alert dialog title
+            builder.setTitle("App background color")
+
+            // Display a message on alert dialog
+            builder.setMessage("Are you want to set the app background color to RED?")
+
+            // Set a positive button and its click listener on alert dialog
+            builder.setPositiveButton("YES"){dialog, which ->
+                // Do something when user press the positive button
+                Toast.makeText(applicationContext,"Ok, we change the app background.",Toast.LENGTH_SHORT).show()
+
+                // Change the app background color
+                change_theme.setBackgroundColor(RED)
+            }
+
+
+            // Display a negative button on alert dialog
+            builder.setNegativeButton("No"){dialog,which ->
+                Toast.makeText(applicationContext,"You are not agree.",Toast.LENGTH_SHORT).show()
+            }
+
+
+            // Display a neutral button on alert dialog
+            builder.setNeutralButton("Cancel"){_,_ ->
+                Toast.makeText(applicationContext,"You cancelled the dialog.",Toast.LENGTH_SHORT).show()
+            }
+
+            // Finally, make the alert dialog using builder
+            val dialog: AlertDialog = builder.create()
+
+            // Display the alert dialog on app interface
+            dialog.show()
+        }
+
+
     }
 }
+fun popChangeThemeDialog(view: View) {
+
+}
+
+
---
+++ app/src/main/java/com/example/boardgamehelper/ThemeChangeDialog.kt
@@ -0,0 +1,4 @@
+package com.example.boardgamehelper
+
+class ThemeChangeDialog  {
+}
