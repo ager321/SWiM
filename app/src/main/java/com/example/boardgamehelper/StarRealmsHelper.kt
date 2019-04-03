071143beb01c8ef7a11fa9858f647c403d55f05d
Paweł <paweltraw@gmail.com>
2019-04-03 19:33:07 +0200

    fragment dialog added

--- app/src/main/java/com/example/boardgamehelper/StarRealmsHelper.kt
+++ app/src/main/java/com/example/boardgamehelper/StarRealmsHelper.kt
@@ -33,46 +33,30 @@ class StarRealmsHelper : AppCompatActivity() {
         change_theme.setOnClickListener{
             // Initialize a new instance of
             val builder = AlertDialog.Builder(this@StarRealmsHelper)
+            builder.setTitle("Button background color")
+            builder.setMessage("Are you want to set the button background color to RED?")
 
-            // Set the alert dialog title
-            builder.setTitle("App background color")
-
-            // Display a message on alert dialog
-            builder.setMessage("Are you want to set the app background color to RED?")
-
-            // Set a positive button and its click listener on alert dialog
             builder.setPositiveButton("YES"){dialog, which ->
-                // Do something when user press the positive button
-                Toast.makeText(applicationContext,"Ok, we change the app background.",Toast.LENGTH_SHORT).show()
-
-                // Change the app background color
                 change_theme.setBackgroundColor(RED)
             }
 
-
-            // Display a negative button on alert dialog
             builder.setNegativeButton("No"){dialog,which ->
                 Toast.makeText(applicationContext,"You are not agree.",Toast.LENGTH_SHORT).show()
             }
-
-
-            // Display a neutral button on alert dialog
             builder.setNeutralButton("Cancel"){_,_ ->
                 Toast.makeText(applicationContext,"You cancelled the dialog.",Toast.LENGTH_SHORT).show()
             }
-
-            // Finally, make the alert dialog using builder
             val dialog: AlertDialog = builder.create()
-
-            // Display the alert dialog on app interface
             dialog.show()
         }
 
+        button.setOnClickListener {
+        }
+        val ft = getSupportFragmentManager().beginTransaction()
+        val newFragment = ThemeChangeDialog.newInstance("content")
+        newFragment.show(ft, "dialog")
 
     }
-}
-fun popChangeThemeDialog(view: View) {
 
 }
-
 
--- app/src/main/java/com/example/boardgamehelper/ThemeChangeDialog.kt
+++ app/src/main/java/com/example/boardgamehelper/ThemeChangeDialog.kt
@@ -1,4 +1,84 @@
 package com.example.boardgamehelper
 
-class ThemeChangeDialog  {
+import android.app.Activity
+import android.content.*
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+
+/**
+ * Created by Mobidroidtech on 3/14/2018.
+ */
+class ThemeChangeDialog : DialogFragment() {
+
+    private var content: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        content = arguments?.getString("content")
+
+        // Pick a style based on the num.
+        val style = DialogFragment.STYLE_NO_FRAME
+        val theme = R.style.DialogTheme
+        setStyle(style, theme)
+    }
+
+    // Override the Fragment.onAttach() method to instantiate the
+    // NoticeDialogListener
+    override fun onAttach(activity: Activity?) {
+        super.onAttach(activity)
+
+
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.layout_dialog, container, false)
+
+        val btnCancel = view.findViewById<View>(R.id.buttonCancel) as Button
+        val btnAccept = view.findViewById<View>(R.id.buttonAccept) as Button
+
+        val textViewContent = view.findViewById<View>(R.id.textViewContent) as TextView
+        textViewContent.text = content
+
+
+        btnCancel.setOnClickListener {
+            Toast.makeText(activity, "action cancelled", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
+
+        btnAccept.setOnClickListener {
+            Toast.makeText(activity, "User Accepted Action", Toast.LENGTH_SHORT).show()
+            dismiss()
+        }
+
+        return view
+    }
+
+    companion object {
+
+
+        /**
+         * Create a new instance of CustomDialogFragment, providing "num" as an
+         * argument.
+         */
+        fun newInstance(content: String): ThemeChangeDialog {
+            val f = ThemeChangeDialog()
+
+            // Supply num input as an argument.
+            val args = Bundle()
+            args.putString("content", content)
+            f.arguments = args
+
+            return f
+        }
+    }
+
 }
